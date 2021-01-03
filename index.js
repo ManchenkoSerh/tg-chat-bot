e9977eb0580a8000f3661677822cc310d42a9c68
Serhii Manchenko <grandx.go@gmail.com>
2021-01-03 15:55:24 +0200

    add helmet

--- index.js
+++ index.js
@@ -1,21 +1,28 @@
 const express = require('express')
 const cors = require('cors')
+const pino = require('pino');
+const expressPino = require('express-pino-logger');
 const { Bot } = require('tgapi')
 const multer = require('multer');
+const helmet = require('helmet');
 const fs = require('fs')
 const path = require('path');
 
 const app = express()
-const port = 3333
+const logger = pino({ level: 'info' });
+const expressLogger = expressPino({ logger });
+const port = process.env.PORT || 3333;
 const bot = new Bot('1436481545:AAGH14yceC80CPlb12szxJ2vQ7xZTSBMVsA')
 const chat_id = '-426861819'
 
+app.use(helmet());
+app.use(expressLogger);
 app.use(cors())
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        if(!fs.existsSync('./uploads')){
+        if (!fs.existsSync('./uploads')) {
             fs.mkdirSync('./uploads')
         }
         cb(null, 'uploads');
@@ -35,6 +42,12 @@ const fileFilter = (req, file, cb) => {
 
 const upload = multer({ storage: storage, fileFilter: fileFilter });
 
+const unlinkFile = (path) => {
+    fs.unlink(path, (err) => {
+        if (err) throw err;
+    })
+}
+
 app.post('/sendForm', upload.single('image'), async (req, res) => {
     if (req.fileValidationError) {
         return res.status(400).send('Form supports only jpeg and png files');
@@ -42,21 +55,18 @@ app.post('/sendForm', upload.single('image'), async (req, res) => {
     if (req.file) {
         const photo = fs.createReadStream(req.file.path)
         const caption = `firstname: ${req.body.firstName}\nlastname: ${req.body.lastName}`
-        const sended = await bot.sendPhoto({ chat_id, photo, caption})
+        const sended = await bot.sendPhoto({ chat_id, photo, caption })
+        logger.info(sended)
         if (sended.ok) {
-            fs.unlink(req.file.path, (err) => {
-                if (err) throw err;
-            })
+            unlinkFile(req.file.path)
             res.status(200).end()
         } else {
-            fs.unlink(req.file.path, (err) => {
-                if (err) throw err;
-            })
+            unlinkFile(req.file.path)
             res.status(sended.error_code).send(sended.description)
         }
 
     }
-    
+
 })
 
 app.all('*', (req, res) => {
